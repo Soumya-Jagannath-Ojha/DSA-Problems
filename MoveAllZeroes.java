0b381dddf8714a43e841a23ecf0617be6c0c1265
Soumya <soumyajagannathojha7@gmail.com>
2024-11-16 19:43:45 +0530

    pushZerosToEnd

---
+++ MoveAllZeroes.java
@@ -0,0 +1,24 @@
+public class MoveAllZeroes {
+
+    public static void pushZerosToEnd(int[] arr) {
+        // Two pointer approach
+        //  i == 0 (ignore)
+        //  i != 0 (swap)
+        
+        int i =0;
+        for(int j=0; j<arr.length; j++){
+            if(arr[j] != 0){
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+                i++;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {1,0,2,3};
+        pushZerosToEnd(arr);
+    }
+}
